1b43309346138f483dbe3add23c9c964521214a2
Berk Seker <aberkseker@outlook.com>
2026-06-21 16:10:46 +0300

    Basit hesap makine projesi eklendi

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
-﻿// Değişken tanımlama 
-string isim = "Berk";
-int yas = 24;
+﻿Console.WriteLine("=== Basit Hesap Makinesi == ");
 
-// Console.WriteLine ekrana yazdırır
-Console.WriteLine($"Merhaba. {isim}! Yaşin: {yas}");
+Console.WriteLine("Birinci Sayiyi gir: ");
+double sayi1 = Convert.ToDouble(Console.ReadLine());
 
-// Kullanıcıdan girdi almak
-Console.WriteLine("Adini gir: ");
-string kullaniciAdi = Console.ReadLine() ?? "";
+Console.WriteLine("İslem Sec (+, -, *, /):");
+string islem = Console.ReadLine() ?? "";
 
-Console.WriteLine($"Hoş Geldin, {kullaniciAdi}!");
+Console.WriteLine("İkinci sayiyi gir: ");
+double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+double sonuc;
+
+switch (islem)
+{
+    case "+":
+        sonuc = sayi1 + sayi2;
+        break;
+    case "-":
+        sonuc = sayi1 - sayi2;
+        break;
+    case "*":
+        sonuc = sayi1 * sayi2;
+        break;
+    case "/":
+        if (sayi2 == 0)
+        {
+            Console.WriteLine("Hata: Sifira bolme yapilamaz!");
+            return;
+        }
+        sonuc = sayi1 / sayi2;
+        break;
+    default: 
+        Console.WriteLine("Gecersiz İslem!");
+        return;
+}
+Console.WriteLine($"Sonuc: {sayi1} {islem} {sayi2} = {sonuc}");
