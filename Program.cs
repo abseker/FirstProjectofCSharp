254c1c423386b8c3a5ec48d75bf109f7006db86c
Berk Seker <aberkseker@outlook.com>
2026-06-21 15:42:22 +0300

    Ilk C# konsol uygulamasi

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Değişken tanımlama 
+string isim = "Berk";
+int yas = 24;
+
+// Console.WriteLine ekrana yazdırır
+Console.WriteLine($"Merhaba. {isim}! Yaşin: {yas}");
+
+// Kullanıcıdan girdi almak
+Console.WriteLine("Adini gir: ");
+string kullaniciAdi = Console.ReadLine() ?? "";
+
+Console.WriteLine($"Hoş Geldin, {kullaniciAdi}!");
